2bd1c1c0777144d76eecb043beda3fa6efa4c442
Benjamin Tayar <tayarbenjamin@gmail.com>
2023-11-14 11:36:02 -0300

    solved

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,22 @@
+class GamesController < ApplicationController
+
+def new
+range de a a Z
+sample de 10
+
+
+def score
+  @words = params[:word]
+
+  
+
+  # The word can’t be built out of the original grid
+  # The word is valid according to the grid, but is not a valid English word
+  # The word is valid according to the grid and is an English word
+
+
+
+end
+
+
+end
